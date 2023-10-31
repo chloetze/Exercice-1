08ab218348e15f3fe18fe6bf7a5557c171d64526
chloe <chloe@LAPTOP-5MH8IVM1>
2023-10-31 11:41:38 +0100

    Ajoutez des fichiers projet.

---
+++ Exercice 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclarations et initialisations
+            string prenom = "Alain";
+            string nom = "DUPONT";
+            int age = 19;
+
+            // saisie des 3 informations
+            Console.Write("Alain");
+            prenom = Console.ReadLine();
+            Console.Write("DUPONT");
+            nom = Console.ReadLine();
+            Console.Write("19");
+            age = int.Parse(Console.ReadLine());
+           
+            // afficahge personnalisé
+            Console.WriteLine("Bonjour" + prenom + " " + nom + ", vous avez " + age + "ans.");
+            Console.ReadLine();
+
+        }
+    }
+}
